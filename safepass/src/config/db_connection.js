e24adc6b73140f580b8f5afce161891e0b64ff7f
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-02-14 23:34:17 +0530

    All the configuration of Database.

---
+++ safepass/src/config/db_connection.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+
+// MySQL database connection credentials
+const configcredential = {
+    host: process.env.DB_HOST,       // Update if using a remote database
+    user: process.env.DB_USER,            // MySQL username
+    password:process.env.DB_PASSWORD,      // MySQL password
+    database: process.env.DB_NAME, // Database name
+    port: process.env.DB_PORT,              // Default MySQL port
+    multipleStatements: process.env.DB_MULTI_STATEMENT
+};
+
+// Function to get a new MySQL connection
+export async function getDBConnection() {
+    return await mysql.createConnection(configcredential);
+}
