51cb5e377db69b3f5ce44dbd3e525b2ef4a032be
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-02-14 23:35:46 +0530

    Middleware.js is the file for the clerk.js settings and configurations

--- safepass/src/middleware.js
+++ safepass/src/middleware.js
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/'])
+const isPublicRoute = createRouteMatcher(['/', '/api/user'])
 // sign-in(.*)
 
 export default clerkMiddleware(async (auth, request) => {
