f46723f27dedd56fcebf95a33fc1bdaa80ad270e
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-02-17 23:54:38 +0530

    altered the code and added the more coding related statement to get the data in the best form. In the altered code I added the function such as encryption and decryption at the backend level so that user's data should be safe and secure

--- safepass/src/app/api/addcard/route.js
+++ safepass/src/app/api/addcard/route.js
@@ -1,17 +1,54 @@
 import { NextResponse } from "next/server";
 import { getDBConnection } from "@/config/db_connection";
+import { BEdecrypt, BEencrypt} from "@/utils/encryption";
+import { Bed } from "lucide-react";
 // import { useEffect } from "react";
 
+
+export async function GET(req) {
+    try {
+      const { searchParams } = new URL(req.url);
+      const userid = searchParams.get("userid");
+  
+      if (!userid) {
+        return NextResponse.json({ message: "Email is required" }, { status: 400 });
+      }
+      const connection = await getDBConnection();
+      const [cards] = await connection.execute("SELECT * FROM userCards WHERE user_id = ?", [userid]);
+      await connection.end();
+  
+      if (cards.length === 0) {
+        return NextResponse.json({ message: "Cards not found" }, { status: 404 });
+      }
+      const decryptedCards = cards.map(card => ({
+        ...card,
+        pin:BEdecrypt(card.pin),cvv:BEdecrypt(card.cvv), expiry_date:BEdecrypt(card.expiry_date) // Decrypt pin before sending responsedecrypt(user.pin)
+      }));
+      return NextResponse.json(decryptedCards); // Return user details
+    } catch (error) {
+      console.error("❌ Fetch User Error:", error);
+      return NextResponse.json({ message: "Internal Server Error", error: error.message }, { status: 500 });
+    }
+  }
+
+
+
+
+  
+// -----------------------------------------------------------------------------------------------------------------
 export async function POST(req) {
     try {
-        const { user_id, cardNumber,cardholderName, expiryDate, cvv, pin, network, bank, cardType }=await req.json();
-        if(!cardNumber || !cardholderName ||!expiryDate ||!cvv||!pin||!network || !bank || !cardType){
+        let { user_id, cardNumber,cardholderName, expiryDate, cvv, pin, network, bank, cardType }=await req.json();
+        if(!user_id || !cardNumber || !cardholderName ||!expiryDate ||!cvv||!pin||!network || !bank || !cardType){
             return NextResponse.json({
                 message:"All Fields are Required"
             }, {
                 status:400
             });
         }
+        pin=BEencrypt(pin);
+        cvv=BEencrypt(cvv);
+        expiryDate=BEencrypt(expiryDate);
         const connection =await getDBConnection();
         await connection.execute("INSERT INTO userCards (user_id, pin,cvv,expiry_date,card_no,cardholder_name,type,card_network,bank_name) VALUES (?,?,?,?,?,?,?,?,?)",
            
