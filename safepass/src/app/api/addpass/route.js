336652b8d7b9bbdaad3f5ecb104aba8efaddcc6e
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-02-17 23:55:02 +0530

    altered the code and added the more coding related statement to get the data in the best form. In the altered code I added the function such as encryption and decryption at the backend level so that user's data should be safe and secure

--- safepass/src/app/api/addpass/route.js
+++ safepass/src/app/api/addpass/route.js
@@ -1,11 +1,43 @@
 import { NextResponse } from "next/server";
 import { getDBConnection } from "@/config/db_connection";
+import { BEdecrypt, BEencrypt } from "@/utils/encryption";
 
 
+
+export async function GET(req) {
+    try {
+      const { searchParams } = new URL(req.url);
+      const userid = searchParams.get("userid");
+  
+      if (!userid) {
+        return NextResponse.json({ message: "Email is required" }, { status: 400 });
+      }
+  
+      const connection = await getDBConnection();
+      const [passwords] = await connection.execute("SELECT * FROM userPassword WHERE user_id = ?", [userid]);
+      await connection.end();
+  
+      if (passwords.length === 0) {
+        return NextResponse.json({ message: "User not found" }, { status: 404 });
+      }
+
+      const decryptedPassword = passwords.map(password => ({
+        ...password,
+        password_hashed:BEdecrypt(password.password_hashed) // Decrypt pin before sending responsedecrypt(user.pin)
+      }));
+      return NextResponse.json(decryptedPassword); // Return user details
+    } catch (error) {
+      console.error("❌ Fetch User Error:", error);
+      return NextResponse.json({ message: "Internal Server Error", error: error.message }, { status: 500 });
+    }
+  }
+
+
+// -------------------------------------------------------------------------------------------------------
 export async function POST(req) {
     try {
         
-        const { user_id ,username, websiteName, url, password, confirmPassword}=await req.json();
+        let { user_id ,username, websiteName, url, password, confirmPassword}=await req.json();
         if(!user_id || !username ||!websiteName ||!url||!password||!confirmPassword){
             return NextResponse.json({
                 message:"All Fields are Required"
@@ -13,6 +45,7 @@ export async function POST(req) {
                 status:400
             });
         }
+        password=BEencrypt(password);
         const connection =await getDBConnection();
         await connection.execute("INSERT INTO userPassword (user_id,username, website_name, url, password_hashed) VALUES (?,?,?,?,?)",
             [user_id, username, websiteName,url, password]);
