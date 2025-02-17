92cae7b7ca95e59aa7efff2d63520ce6f263a1c3
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2025-02-17 23:50:39 +0530

    altered the file to achieve the best design and performance.

--- safepass/src/components/navbar.js
+++ safepass/src/components/navbar.js
@@ -1,73 +1,3 @@
-// 'use client'
-// import { Moon, MoonStar, Sun } from 'lucide-react'
-// import { useTheme } from 'next-themes'
-// import React from 'react';
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu"
-// import { Button } from './ui/button'
-// import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/nextjs';
-// import { useUserContext } from '@/context/userContext';
-
-// // -----------------------------------------------------------------------------------------------
-// const navbar = () => {
-//   const { userdata }=useUserContext();
-//   const { setTheme } = useTheme();
-//   return (
-//     <>
-//       <nav className='flex justify-between px-8 h-24 bg-yellow-400 items-center backdrop-brightness-50'>
-//         <div>
-//           <h1 className='font-bold text-2xl text-pretty'><span className='text-gray-600'>S</span>afe<span className='text-gray-600'>P</span>ass</h1>
-//           <p>Your <span className='text-slate-500'>P</span>assword Manager</p>
-
-//         </div>
-//         <ul className='flex flex-row gap-3 text-xl'>
-//           <li>Home</li>
-//           <li>About</li>
-//           <li>{userdata.name}{userdata.email}</li>
-//         </ul>
-
-        
-//         <div>
-          
-//         <SignedOut>
-//             <SignInButton />
-//           </SignedOut>
-//           <SignedIn>
-//             <UserButton />
-//           </SignedIn>
-          
-//           <DropdownMenu>
-//             <DropdownMenuTrigger asChild>
-//               <Button variant="outline" size="icon">
-//                 <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-//                 <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-//                 <span className="sr-only">Toggle theme</span>
-//               </Button>
-//             </DropdownMenuTrigger>
-//             <DropdownMenuContent align="end">
-//               <DropdownMenuItem onClick={() => setTheme("light")}>
-//                 Light
-//               </DropdownMenuItem>
-//               <DropdownMenuItem onClick={() => setTheme("dark")}>
-//                 Dark
-//               </DropdownMenuItem>
-//               <DropdownMenuItem onClick={() => setTheme("system")}>
-//                 System
-//               </DropdownMenuItem>
-//             </DropdownMenuContent>
-//           </DropdownMenu>
-//         </div>
-        
-//       </nav>
-//     </>
-//   )
-// }
-
-// export default navbar
 "use client";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
@@ -78,41 +8,28 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton, SignUpButton } from "@clerk/nextjs";
 import { useUserContext } from "@/context/userContext";
 
 const Navbar = () => {
   const { userdata } = useUserContext();
   const { setTheme } = useTheme();
 
-  return (
-    <nav className="flex justify-between px-8 h-24 bg-yellow-400 items-center backdrop-brightness-50">
-      <div>
-        <h1 className="font-bold text-2xl text-pretty">
-          <span className="text-gray-600">S</span>afe
-          <span className="text-gray-600">P</span>ass
-        </h1>
-        <p>
-          Your <span className="text-slate-500">P</span>assword Manager
-        </p>
-      </div>
-
-      <ul className="flex flex-row gap-3 text-xl">
-        <li>Home</li>
-        <li>About</li>
-        <li>
-          {userdata ? `${userdata.name} (${userdata.email})` : "Loading..."}
-        </li>
-      </ul>
-
-      <div>
-        <SignedOut>
-          <SignInButton />
+  return (<>
+    {/* // ------------------------------------------------- */}
+    <nav className="p-6 flex justify-between items-center bg-gradient-to-tr to-purple-900 from-blue-900 border-b-2">
+      <h1 className="text-2xl font-bold">
+        <span className="text-slate-300">S</span>afe
+        <span className="text-slate-300">P</span>ass
+      </h1>
+      <div className="flex space-x-4">
+      <SignedOut>
+          <SignInButton className="bg-accent text-accent-foreground hover:bg-accent/80 py-3 px-6 rounded-lg" />
+          <SignUpButton className="bg-accent text-accent-foreground hover:bg-accent/80 py-3 px-6 rounded-lg"  />
         </SignedOut>
         <SignedIn>
-          <UserButton />
+          <UserButton showName={true} />
         </SignedIn>
-
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" size="icon">
@@ -129,6 +46,12 @@ const Navbar = () => {
         </DropdownMenu>
       </div>
     </nav>
+
+
+    {/* // ------------------------------------------------- */}
+
+
+    </>
   );
 };
 
